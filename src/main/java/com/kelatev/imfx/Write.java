c4b7f4c86f47e9af3d57f46c82ef036fc4de9346
Kelatev <kelatev@gmail.com>
2016-07-27 00:05:38 +0300

    00.05

--- src/main/java/com/kelatev/imfx/Write.java
+++ src/main/java/com/kelatev/imfx/Write.java
@@ -1,5 +1,46 @@
 package com.kelatev.imfx;
 
+import com.kelatev.imfx.doclist.DocList;
+import com.kelatev.imfx.envelope.Envelope;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
 public class Write {
+
+
+    private InputStream writeFile(InputStream imfx, InputStream fileStream, String fileName) {
+        return writeFile(imfx, fileStream, fileName, null);
+    }
+    private InputStream writeFile(InputStream imfx, InputStream fileStream, String fileName, InputStream sign) {
+        return null;
+    }
+
+
+    private InputStream addDoclist(InputStream imfx, DocList docList) {
+        return addDoclist(imfx, docList);
+    }
+    private InputStream addDoclist(InputStream imfx, DocList docList, InputStream sign) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+
+
+        InputStream fileStream = new ByteArrayInputStream(baos.toByteArray());
+        return writeFile(imfx, fileStream, Constant.DOCLIST_FILE_NAME, sign);
+    }
+
+
+    private InputStream addEnvelope(InputStream imfx, Envelope envelope) {
+        return addEnvelope(imfx, envelope);
+    }
+    private InputStream addEnvelope(InputStream imfx, Envelope envelope, InputStream sign) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+
+
+        InputStream fileStream = new ByteArrayInputStream(baos.toByteArray());
+        return writeFile(imfx, fileStream, Constant.ENVELOPE_FILE_NAME, sign);
+    }
 
 }
